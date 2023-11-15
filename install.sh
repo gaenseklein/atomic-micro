d9468c3311d7934a47f6fdf85cfb5b14263532d5
gaenseklein <dekan@gaenseklein.net>
2023-11-15 20:55:20 +0100

    fixed errors

--- install.sh
+++ install.sh
@@ -5,10 +5,12 @@ micro -plugin install quoter
 micro -plugin install filemanager
 micro -plugin install bookmark
 micro -plugin install snippets
-cp bindings.json ~/config/micro/bindings.json
-cp settings.json ~/config/micro/settings.json
-cd ~/config/micro/plug
+cp bindings.json ~/.config/micro/bindings.json
+cp settings.json ~/.config/micro/settings.json
 git clone https://github.com/gaenseklein/repfiles
 git clone https://github.com/gaenseklein/grepsearch
 git clone https://github.com/gaenseklein/gittab
+mv repfiles ~/.config/micro/
+mv grepsearch ~/.config/micro/
+mv gittab ~/.config/micro/
 echo "all done, happy microing"
